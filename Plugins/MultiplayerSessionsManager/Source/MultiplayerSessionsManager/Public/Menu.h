323480a6a113514c57afa4028d025482e6c94232
Umar Amin <79603472+OGdevsol@users.noreply.github.com>
2023-01-22 14:15:58 +0500

    Initial Submission
    Github Initialization for the project

---
+++ Plugins/MultiplayerSessionsManager/Source/MultiplayerSessionsManager/Public/Menu.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HAL/Platform.h"
+#include "HAL/Platform.h"
+#include "HAL/Platform.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Menu.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSESSIONSMANAGER_API UMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+public:
+	UFUNCTION(BlueprintCallable)
+		void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")), FString LobbyPath = FString(TEXT("/Game/ThirdPerson/Maps/Lobby")));
+
+
+protected:
+	virtual bool Initialize() override;
+	virtual void NativeDestruct() override;
+	//
+	// Callbacks for custom delegates on MultiplayerSessionsSubsystem
+	//
+	UFUNCTION()
+	void OnCreateSession(bool bWasSuccessful);
+	void OnFindSessions(const TArray<FOnlineSessionSearchResult>& SessionResults, bool bWasSuccessful);
+	void OnJoinSession(EOnJoinSessionCompleteResult::Type Result);
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessful);
+	UFUNCTION()
+	void OnStartSession(bool bWasSuccessful);
+
+
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UButton* HostButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* JoinButton;
+
+	UFUNCTION()
+		void HostButtonClicked();
+
+	UFUNCTION()
+		void JoinButtonClicked();
+
+	void MenuTearDown();
+
+	//Subsystem Designed to handle online functionality for sessions
+	class UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
+
+	int32 NumPublicConnections{ 4 };
+	FString MatchType{ TEXT("FreeForAll") };
+FString PathToLobby {TEXT("")};
+	FString GName {TEXT("EnterGNameHere")};
+
+	
+};
