040243b8e51a360350e97840d3df2c2bc1fcb03f
Umar Amin <79603472+OGdevsol@users.noreply.github.com>
2023-01-22 21:11:30 +0500

    New Additions
    1) Added a Camera and a Spring Arm component
    2) Character movement methods and implementation
    3) Axis and Action mappings
    4)Animinstance
    5)State machine for idle_walk_run anims
    6)Level polish
    7)Transition level

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -2,33 +2,75 @@
 
 
 #include "BlasterCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
-// Sets default values
 ABlasterCharacter::ABlasterCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-}
-
-// Called when the game starts or when spawned
-void ABlasterCharacter::BeginPlay()
-{
-	Super::BeginPlay();
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->bUsePawnControlRotation = true;
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom,USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false; // false because it is attached to the CameraBoom with PawnControlRotation set to true
+	bUseControllerRotationYaw=false;
+	GetCharacterMovement()->bOrientRotationToMovement=true;
 	
 }
 
-// Called every frame
-void ABlasterCharacter::Tick(float DeltaTime)
+void ABlasterCharacter::BeginPlay()
 {
-	Super::Tick(DeltaTime);
-
+	Super::BeginPlay();
 }
-
-// Called to bind functionality to input
 void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump",IE_Pressed,this,&ACharacter::Jump);
+	PlayerInputComponent->BindAxis("MoveForward",this,&ABlasterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight",this,&ABlasterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn",this,&ABlasterCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp",this,&ABlasterCharacter::LookUp);
+	
+}
 
+
+void ABlasterCharacter::MoveForward(float Value) //Make sure controller isn't null and value isn't zero. Character class has an inherited variable "Controller" type AController
+{
+	if (Controller!=nullptr || Value!=0.f)
+	{
+		const FRotator YawRotation(0.0f,Controller->GetControlRotation().Yaw,0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction,Value);
+	}
+	
+}
+
+void ABlasterCharacter::MoveRight(float Value)
+{
+	if (Controller!=nullptr || Value!=0.f)
+	{
+		const FRotator YawRotation(0.0f,Controller->GetControlRotation().Yaw,0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction,Value);
+	}
+}
+
+void ABlasterCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ABlasterCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void ABlasterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 }
 
